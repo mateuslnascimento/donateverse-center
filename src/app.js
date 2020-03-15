b73f97c6a3ee13bf54bb0130eeb9c64c4150dabd
Mateus Nascimento <mateusl1n1@gmail.com>
2020-03-15 20:52:22 -0300

    Adicionando dotEnv

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 'use strict';
 
+const dotEnv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/microservices?readPreference=primary';
-
 const app = express();
 const router = express.Router();
 
-console.log(connectionString);
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const {DonationCenter} = require('./models/donationCenter');
 
